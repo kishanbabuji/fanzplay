e30c85b01870183e250105bdde738298549d311c
tomacam <tomacam@live.unc.edu>
2022-03-01 11:50:22 -0500

    Fix firebase and add to sign up page

--- components/signup.js
+++ components/signup.js
@@ -1,66 +1,184 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { useState } from 'react';
-import * as React from 'react';
+import { StyleSheet, View, TextInput } from "react-native";
+import { useState } from "react";
+import * as React from "react";
 
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
+import { auth, db } from "../firebase/firebaseClient";
+import { TextField, Button, Text, Colors } from "react-native-ui-lib";
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Modal,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [zip, setZip] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  async function signupWithEmail() {
+    // const auth = await getAuth();
 
-    async function signupWithEmail() {
-        const auth = await getAuth();
+    // createUserWithEmailAndPassword(auth, email, password).then(cred => {
+    //     return firestore.collection('users').doc(cred.user.uid).set({
+    //         Name: name
+    //     })
+    // })
 
-        createUserWithEmailAndPassword(auth, email, password)
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((cred) => {
+        db.collection("users")
+          .doc(cred.user.uid)
+          .set({
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            zipCode: zip,
+          })
+          .then(
+            setEmail(""),
+            setFirstName(""),
+            setLastName(""),
+            setPassword(""),
+            setAge(""),
+            setZip(""),
+            setModalVisible(true)
+          );
+      });
+  }
 
-
-    }
-
-
-
-    return (
-        <View style={styles.container}>
-            <Text> fans play sign up page </Text>
-            <TextInput
-                value={email}
-                style={styles.input}
-
-                onChangeText={email => setEmail(email)}
-
-
-
-            />
-            <TextInput
-                style={styles.input}
-                value={password}
-                onChangeText={password => setPassword(password)}
-
-
-            />
-            <Button
-                onPress={signupWithEmail}
-                title="Submit"
-                color="#FFDB58"
-                accessibilityLabel="Learn more about this purple button"
-            />
-
-        </View>
-    );
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <Text text30 style={{ color: Colors.text }}>
+          Sign up for FANz PLAY
+        </Text>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={{ color: Colors.text }}>
+                Congratulations! Your FANz PLAY account has been created!
+              </Text>
+              <Button
+                backgroundColor={Colors.text}
+                onPress={() => setModalVisible(!modalVisible)}
+                label={"Go Home!"}
+                enableShadow
+              ></Button>
+            </View>
+          </View>
+        </Modal>
+        <TextField
+          value={email}
+          style={styles.input}
+          enableErrors
+          placeholder={"Email"}
+          floatingPlaceholder
+          onChangeText={(email) => setEmail(email)}
+          textContentType="emailAddress"
+        />
+        <TextField
+          style={styles.input}
+          placeholder={"Password"}
+          floatingPlaceholder
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+        />
+        <TextField
+          value={firstName}
+          style={styles.input}
+          placeholder={"First Name"}
+          floatingPlaceholder
+          onChangeText={(firstName) => setFirstName(firstName)}
+        />
+        <TextField
+          value={lastName}
+          style={styles.input}
+          placeholder={"Last Name"}
+          floatingPlaceholder
+          onChangeText={(lastName) => setLastName(lastName)}
+        />
+        <TextField
+          value={age}
+          style={styles.input}
+          placeholder={"Age"}
+          floatingPlaceholder
+          onChangeText={(age) => setAge(age)}
+          keyboardType="numeric"
+        />
+        <TextField
+          value={zip}
+          style={styles.input}
+          placeholder={"Zip Code"}
+          floatingPlaceholder
+          onChangeText={(zip) => setZip(zip)}
+          keyboardType="numeric"
+        />
+        <Button
+          onPress={signupWithEmail}
+          label={"Submit"}
+          backgroundColor={Colors.text}
+          accessibilityLabel="Learn more about this purple button"
+          enableShadow
+        />
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    // height: 40,
+    // margin: 12,
+    width: 200,
+    // borderWidth: 1,
+    // padding: 0,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    input: {
-        height: 40,
-        margin: 12,
-        width: 200,
-        borderWidth: 1,
-        padding: 10,
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
+
+Colors.loadColors({
+  text: "#879428",
 });
--- firebase/firebaseClient.js
+++ firebase/firebaseClient.js
@@ -4,6 +4,7 @@ import { FirebaseOptions, getApp, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 import { PUBLIC_FIREBASE_API_KEY, PUBLIC_FIREBASE_AUTH_DOMAIN, PUBLIC_FIREBASE_PROJECT_ID, PUBLIC_FIREBASE_STORAGE_BUCKET, PUBLIC_FIREBASE_MESSAGING_SENDER_ID, PUBLIC_FIREBASE_APP_ID } from "@envLocal";
+import firebase from 'firebase/compat';
 
 const clientCredentials = {
     apiKey: PUBLIC_FIREBASE_API_KEY,
@@ -16,20 +17,25 @@ const clientCredentials = {
 
 
 
-function createFirebaseApp(config) {
-    try {
-        return getApp();
-    } catch {
-        let app = initializeApp(config);
+// function createFirebaseApp(config) {
+//     try {
+//         return getApp();
+//     } catch {
+//         let app = initializeApp(config);
 
-        return app
-    }
-}
-
-
+//         return app
+//     }
+// }
 
 
-//firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
 
 
-export const firebase = createFirebaseApp(clientCredentials);
+// //firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+
+
+// export const firebase = createFirebaseApp(clientCredentials);
+
+firebase.initializeApp(clientCredentials);
+export const auth=firebase.auth;
+export const db=firebase.firestore();
+export const timestamp=firebase.firestore.Timestamp;
