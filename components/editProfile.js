f6614176ac35fb95a3472303cca182fe2d893198
markaiello <59695132+markaiello@users.noreply.github.com>
2022-03-16 15:55:05 -0400

    requested changes to signup / edit profile

--- components/editProfile.js
+++ components/editProfile.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View, TextInput,Picker } from "react-native";
 import { useState } from 'react';
 import * as React from 'react';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
@@ -11,31 +11,38 @@ import {
     Keyboard,
     Modal,
   } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 
 
-const docRef = doc(db, "users","DhKJj8PPgxYdGlqi7aoCMHI02x92");
+const docRef = doc(db, "users","PmE1VHWO8DbJiFaydZshiPgSUHM2");
 
 
 export default function Edit() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [age, setAge] = useState("");
-    const [zip, setZip] = useState("");
-    const [modalVisible, setModalVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [zip, setZip] = useState("");
+  const [city, setCity] = useState("");
+  const [username, setUsername] = useState("");
+  const [number, setNumber] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
 
 
         React.useEffect(async () => {
             const docSnap = await getDoc(docRef);
     
         if (docSnap.exists()) {
-         
            setFirstName(docSnap.data().firstName)
            setLastName(docSnap.data().lastName)
            setAge(docSnap.data().age) 
            setZip(docSnap.data().zipCode) 
+           setCity(docSnap.data().city)
+           setNumber(docSnap.data().number)
+           setUsername(docSnap.data().username)
     
           } else {
             console.log("No such document!");
@@ -47,16 +54,21 @@ export default function Edit() {
         
       async function signupWithEmail() {
         await updateDoc(docRef, {
-            firstName: firstName,
-            lastName: lastName,
-            age: age,
-            zipCode: zip
+          firstName: firstName,
+          lastName: lastName,
+          age: age,
+          zipCode: zip,
+          city: city,
+          username: username,
+          number: number
 
           });
+          setModalVisible(true)
     
       }
 
     return (
+      <ScrollView>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
           <KeyboardAvoidingView
             behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -81,44 +93,71 @@ export default function Edit() {
                   <Button
                     backgroundColor={Colors.text}
                     onPress={() => setModalVisible(!modalVisible)}
-                    label={"Go Home!"}
+                    label={"Okay!"}
                     enableShadow
                   ></Button>
                 </View>
               </View>
             </Modal>
       
-       
-            <TextField
-              value={firstName}
-              style={styles.input}
-              placeholder={"First Name"}
-              floatingPlaceholder
-              onChangeText={(firstName) => setFirstName(firstName)}
-            />
-            <TextField
-              value={lastName}
-              style={styles.input}
-              placeholder={"Last Name"}
-              floatingPlaceholder
-              onChangeText={(lastName) => setLastName(lastName)}
-            />
-            <TextField
-              value={age}
-              style={styles.input}
-              placeholder={"Age"}
-              floatingPlaceholder
-              onChangeText={(age) => setAge(age)}
-              keyboardType="numeric"
-            />
-            <TextField
-              value={zip}
-              style={styles.input}
-              placeholder={"Zip Code"}
-              floatingPlaceholder
-              onChangeText={(zip) => setZip(zip)}
-              keyboardType="numeric"
-            />
+ 
+        <TextField
+          value={firstName}
+          style={styles.input}
+          placeholder={"First Name"}
+          floatingPlaceholder
+          onChangeText={(firstName) => setFirstName(firstName)}
+        />
+        <TextField
+          value={lastName}
+          style={styles.input}
+          placeholder={"Last Name"}
+          floatingPlaceholder
+          onChangeText={(lastName) => setLastName(lastName)}
+        />
+         <TextField
+          value={username}
+          style={styles.input}
+          placeholder={"Username"}
+          floatingPlaceholder
+          onChangeText={(username) => setUsername(username)}
+        />
+         <TextField
+          value={number}
+          style={styles.input}
+          placeholder={"Phone Number"}
+          floatingPlaceholder
+          onChangeText={(number) => setNumber(number)}
+          keyboardType="numeric"
+        />
+           <Text>Age:</Text>
+        <Picker
+        selectedValue={age}
+        style = {styles.picker}
+        onValueChange={(age, itemIndex) => setAge(age)}
+      >
+        <Picker.Item label="1-13" value="1-13" />
+        <Picker.Item label="14-21" value="14-21" />
+        <Picker.Item label="22-35" value="22-35" />
+        <Picker.Item label="36-50" value="36-50" />
+        <Picker.Item label="51-65" value="51-65" />
+        <Picker.Item label="65+" value="65+" />
+      </Picker>
+        <TextField
+          value={zip}
+          style={styles.input}
+          placeholder={"Zip Code"}
+          floatingPlaceholder
+          onChangeText={(zip) => setZip(zip)}
+          keyboardType="numeric"
+        />
+         <TextField
+          value={city}
+          style={styles.input}
+          placeholder={"City"}
+          floatingPlaceholder
+          onChangeText={(city) => setCity(city)}
+        />
             <Button
               onPress={signupWithEmail}
               label={"Submit"}
@@ -128,10 +167,14 @@ export default function Edit() {
             />
           </KeyboardAvoidingView>
         </TouchableWithoutFeedback>
+        </ScrollView>
       );
 }
 
 const styles = StyleSheet.create({
+  picker: {
+    width: 300,  
+  },
     container: {
       flex: 1,
       backgroundColor: "#FFF",
--- components/signup.js
+++ components/signup.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View, TextInput,Picker,ScrollView } from "react-native";
 import { useState } from "react";
 import * as React from "react";
 
@@ -19,6 +19,9 @@ export default function Signup() {
   const [lastName, setLastName] = useState("");
   const [age, setAge] = useState("");
   const [zip, setZip] = useState("");
+  const [city, setCity] = useState("");
+  const [username, setUsername] = useState("");
+  const [number, setNumber] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
 
   async function signupWithEmail() {
@@ -40,6 +43,9 @@ export default function Signup() {
             lastName: lastName,
             age: age,
             zipCode: zip,
+            city: city,
+            username: username,
+            number: number
           })
           .then(
             setEmail(""),
@@ -48,12 +54,16 @@ export default function Signup() {
             setPassword(""),
             setAge(""),
             setZip(""),
+            setCity(""),
+            setUsername(""),
+            setNumber(""),
             setModalVisible(true)
           );
       });
   }
 
   return (
+    <ScrollView>
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -114,14 +124,36 @@ export default function Signup() {
           floatingPlaceholder
           onChangeText={(lastName) => setLastName(lastName)}
         />
-        <TextField
-          value={age}
+         <TextField
+          value={username}
           style={styles.input}
-          placeholder={"Age"}
+          placeholder={"Username"}
           floatingPlaceholder
-          onChangeText={(age) => setAge(age)}
-          keyboardType="numeric"
+          onChangeText={(username) => setUsername(username)}
         />
+         <TextField
+          value={number}
+          style={styles.input}
+          placeholder={"Phone Number"}
+          floatingPlaceholder
+          onChangeText={(number) => setNumber(number)}
+          keyboardType="numeric"
+        />  
+        <Text>Age:</Text>
+        <Picker
+        selectedValue={age}
+        style = {styles.picker}
+        onValueChange={(age, itemIndex) => setAge(age)}
+      >
+        <Picker.Item label="1-13" value="1-13" />
+        <Picker.Item label="14-21" value="14-21" />
+        <Picker.Item label="22-35" value="22-35" />
+        <Picker.Item label="36-50" value="36-50" />
+        <Picker.Item label="51-65" value="51-65" />
+        <Picker.Item label="65+" value="65+" />
+      </Picker>
+     
+        
         <TextField
           value={zip}
           style={styles.input}
@@ -129,6 +161,13 @@ export default function Signup() {
           floatingPlaceholder
           onChangeText={(zip) => setZip(zip)}
           keyboardType="numeric"
+        />
+         <TextField
+          value={city}
+          style={styles.input}
+          placeholder={"City"}
+          floatingPlaceholder
+          onChangeText={(city) => setCity(city)}
         />
         <Button
           onPress={signupWithEmail}
@@ -139,10 +178,15 @@ export default function Signup() {
         />
       </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  picker: {
+    width: 300,  
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#FFF",
@@ -150,7 +194,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   input: {
-    // height: 40,
+    height: 20,
     // margin: 12,
     width: 200,
     // borderWidth: 1,
